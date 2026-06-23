a50b6b6e36a08b6b2e7575f432d7bf2791d7c73f
mka199580 <mka199580@gmail.com>
2026-06-23 17:07:56 +0300

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+window.EP_SUPABASE_URL = 'https://viueebkvutzjdqhahqvr.supabase.co';
+window.EP_SUPABASE_KEY = 'sb_publishable_vaaEKJTOvIc9MptCul7pgw_zY30P99c';
+window.EP_SUPABASE_BUCKET = 'products';
