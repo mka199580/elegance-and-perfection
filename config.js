f6b1f1c3d67d07db582e0d829f9113ae5db6ff89
mka199580 <mka199580@gmail.com>
2026-06-26 01:16:00 +0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-window.EP_CONFIG = {
-  whatsappNumber: '96555138907',
-  instagramUrl: 'https://www.instagram.com/eleganceandperfection?utm_source=qr',
-  tiktokUrl: 'https://www.tiktok.com/@eleganceandperfection?_r=1&_t=ZS-97SX5yqH4Cb',
-  adminPassword: 'EP2026',
-  currency: 'KWD'
-};
