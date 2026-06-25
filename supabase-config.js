f42afd1681ecc8aa84757985e4da37d9e0add2b5
mka199580 <mka199580@gmail.com>
2026-06-25 23:38:09 +0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,3 +0,0 @@
-window.EP_SUPABASE_URL = 'https://viueebkvutzjdqhahqvr.supabase.co';
-window.EP_SUPABASE_KEY = 'sb_publishable_vaaEKJTOvIc9MptCul7pgw_zY30P99c';
-window.EP_SUPABASE_BUCKET = 'products';
