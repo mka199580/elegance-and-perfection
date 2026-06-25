be2fe5318d9f9f704f96a60c428f5375803c027f
mka199580 <mka199580@gmail.com>
2026-06-26 00:54:47 +0300

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// Elegance & Perfection - Supabase config
+// Replace these if you create a new Supabase project.
+window.EP_SUPABASE_URL = 'https://viueebkvutzjdqhahqvr.supabase.co';
+window.EP_SUPABASE_ANON_KEY = 'sb_publishable_vaaEKJTOvIc9MptCul7pgw_zY30P99c';
+window.EP_WHATSAPP_NUMBER = '96555138907';
+window.EP_STORAGE_BUCKET = 'product-images';
